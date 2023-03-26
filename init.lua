267095e96748939448f9b12363e7b3de9509d205
flux <25628292+fluxionary@users.noreply.github.com>
2023-03-26 12:58:24 -0700

    fix bugs in API calls

--- init.lua
+++ init.lua
@@ -17,8 +17,8 @@ end
 balanced_diet.register_nutrient("fat", { -- raises maximum health, makes you slower
 	apply_value = function(player, value)
 		if value > 0 then
-			hp_attribute:add_max(player, "balanced_nutrients:fat", value / 2)
-			player_monoids.speed:add_change(player, -value / 8, "balanced_nutrients:fat")
+			hp_attribute:add_max(player, "balanced_nutrients:fat", value)
+			player_monoids.speed:add_change(player, 1 - math.min(1 / 6, value / 16), "balanced_nutrients:fat")
 		else
 			hp_attribute:clear_max(player, "balanced_nutrients:fat")
 			player_monoids.speed:del_change(player, "balanced_nutrients:fat")
@@ -37,7 +37,7 @@ balanced_diet.register_nutrient("protein", { -- raises health regeneration, make
 			end
 		else
 			if regen_effect then
-				regen_effect:del_change(player, "balanced_nutrients:protein")
+				regen_effect:clear(player, "balanced_nutrients:protein")
 			end
 			if strength_effect then
 				strength_effect:clear(player, "balanced_nutrients:protein")
@@ -49,13 +49,13 @@ balanced_diet.register_nutrient("protein", { -- raises health regeneration, make
 balanced_diet.register_nutrient("carbohydrate", { -- raises maximum stamina
 	apply_value = function(player, value)
 		if value > 0 and not is_werewolf(player) then
-			stamina_attribute:add_max(player, "balanced_nutrients:carbohydrate", value / 2)
+			stamina_attribute:add_max(player, "balanced_nutrients:carbohydrate", 10 * value)
 		else
-			stamina_attribute:del_max(player, "balanced_nutrients:carbohydrate")
+			stamina_attribute:clear_max(player, "balanced_nutrients:carbohydrate")
 		end
 	end,
 })
-balanced_diet.register_nutrient("vitamin", { -- rises stamina regeneration
+balanced_diet.register_nutrient("vitamin", { -- raises stamina regeneration
 	apply_value = function(player, value)
 		if value > 0 then
 			stamina_regen_effect:add(player, "balanced_nutrients:vitamin", value / 2)
@@ -75,10 +75,10 @@ balanced_diet.register_nutrient("raw_meat", { -- poison for regular players, rai
 	end,
 	apply_value = function(player, value)
 		if value > 0 and is_werewolf(player) then
-			stamina_attribute:add_max(player, "balanced_nutrients:raw_meat", value / 2)
-			stamina_regen_effect:add(player, "balanced_nutrients:raw_meat", value / 2)
+			stamina_attribute:add_max(player, "balanced_nutrients:raw_meat", 10 * value)
+			stamina_regen_effect:add(player, "balanced_nutrients:raw_meat", value)
 		else
-			stamina_attribute:del_max(player, "balanced_nutrients:raw_meat")
+			stamina_attribute:clear_max(player, "balanced_nutrients:raw_meat")
 			stamina_regen_effect:clear(player, "balanced_nutrients:raw_meat")
 		end
 	end,
